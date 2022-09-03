c42559eb5b26cc907dd348efb4f54be08c7712af
YJ Chen <ariel7234@gmail.com>
2022-09-04 00:21:40 +0800

    [add] swiper

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,32 @@
-$(function() {
-  console.log('Hello Bootstrap5');
+var swiper = new Swiper(".swiper", {
+  slidesPerView: 4,
+  spaceBetween: 24,
+  // grabCursor: true,
+  loop: true,
+  autoplay: {
+    delay: 2500,
+    // disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints:{
+    375:{
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    640:{
+      slidesPerView: 2,
+      spaceBetween:20,
+    },
+    768:{
+      slidesPerView: 4,
+      spaceBetween:24
+    }
+  }
 });
